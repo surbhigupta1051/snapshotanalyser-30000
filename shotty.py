ee28a4960700354d08885bbeae762f91e14c8b08
surbhigupta1051 <51705310+surbhigupta1051@users.noreply.github.com>
2019-06-25 18:17:34 +0530

    Update shotty.py

--- shotty.py
+++ shotty.py
@@ -3,11 +3,31 @@ import click
 sesssion = boto3.Session (profile_name ="shotty")
 ec2= session.resource("ec2")
 @click.command()
-for i in ec2.instances.all():
+@click.option('--project',default=None, help ="Only instances for project(tag Project:<name>)")
+def list_instances(project):
+	"list ec2 instances "
+	instances=[]
+	if project:
+		filters = [{'Name':'tag:Project','Values':[project]}]
+		instances = ce2.instances.filter(Filter = filters)
+	else:
+		instances = ec2.instances.all()
+	for i in ec2.instances.all():
 	print(i)
-	print(','.join((
-		i.id,
-		i.instance_type,
-		i.placement['Availability Zone'],
-		i.state['Name']
-		i.public_dns_name)))
+	
+	
+	for i in instances:
+		tags ={t['Key']:t['Value'] for t in i.tag or []}
+		print(','.join((
+			i.id,
+			i.instance_type,
+			i.placement['Availability Zone'],
+			i.state['Name']
+			i.public_dns_name
+			tags.get('Project','<no project>')))
+		      
+		      
+	return
+		      
+if __name__ = ='__main__':
+		      list_instances()
