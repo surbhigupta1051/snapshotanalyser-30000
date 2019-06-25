7116ca632328f1267c5752bd4094bf2c6db13794
surbhigupta1051 <51705310+surbhigupta1051@users.noreply.github.com>
2019-06-25 15:55:50 +0530

    Update shotty.py

--- shotty.py
+++ shotty.py
@@ -1,5 +1,13 @@
 import boto3
+import click
 sesssion = boto3.Session (profile_name ="shotty")
 ec2= session.resource("ec2")
+@click.command()
 for i in ec2.instances.all():
 	print(i)
+	print(','.join((
+		i.id,
+		i.instance_type,
+		i.placement['Availability Zone'],
+		i.state['Name']
+		i.public_dns_name)))
